d1547dcfe66503fc86cc5e9eb714d555a11d8a15
Yingliang <yingliang.24@ichat.sp.edu.sg>
2025-04-30 19:06:21 +0800

    median changed

--- exercise3.py
+++ exercise3.py
@@ -25,12 +25,26 @@ def find_min_max(x):
     print("The maximum temperature is: ", (x[-1]))
     return x
 
+def sort_temperature(x):
+    ascending = x.sort()
+    return ascending 
+
+def calc_median_temp(ascending):
+    
+    if len(ascending) % 2 == 0:
+        median = (ascending[len(ascending)//2] + ascending[len(ascending)//2 - 1]) / 2
+    else:
+        median = ascending[len(ascending)//2]
+    print ("median is " + (median))
+    return 
+    
 
 def main():
     display_main_menu()
     temp = get_user_input()
     calc_average_temperature(temp)
     find_min_max(temp)
-
+    sort_temperature(temp)
+    calc_median_temp(temp)
 if __name__ == "__main__":
     main()
